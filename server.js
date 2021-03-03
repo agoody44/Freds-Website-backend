5d39200bbe555f001c231a200e7b2f3022494bb2
“Alyssa <agoody44@gmail.com>
2021-03-02 21:01:14 -0500

    Sends emails

--- server.js
+++ server.js
@@ -1,128 +1,81 @@
-// Load express
 const express = require('express');
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const nodemailer = require('nodemailer');
 const cors = require('cors');
 
+require('dotenv').config();
 
-require("dotenv").config();
-
-// Create our express app
 const app = express();
 
-
-// Mount middleware (app.use)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
 
 
+app.get('/', (req, res)=>{
+    res.send('welcome to my forma');
+})
 
-// Routes
-app.get('/', function (req, res) {
-    res.send('<h1>Hello World!</h1>');
+app.post('/send', (req,res)=>{
+
+let data = req.body;
+
+let transporter = nodemailer.createTransport({
+    service:'gmail',
+    port:465,
+    secure: true,
+    auth:{
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+    }
 });
 
-app.post('/api/form',(req,res) => {
-    console.log(req.body)
-    let data = req.body
-    let smtpTransport = nodemailer.createTransport({
-        host:'gmail.com',
-        port:465,
-        auth:{
-            user:'process.env.EMAIL',
-            pass:'process.env.PASSWORD'
-        }
-    });
 
 let mailOptions={
     from:data.email,
-    to:'process.env.EMAIL',
+    to: process.env.EMAIL,
     subject:`Message from ${data.name}`,
     html:`
     
-    <h3>Information</h3>
-        <ul>
+    <h3>Informations</h3>
+    <ul>
         <li>Name: ${data.name}</li>
         <li>Lastname: ${data.lastname}</li>
         <li>Email: ${data.email}</li>
-        </ul>
-
-        <h3>Message</h3>
-        <p>${data.message}</p>
+    
+    </ul> 
+    <h3>Message</h3> 
+    <p>${data.message}</p>  
     
     `
+
 };
 
-smtpTransport.sendMail(mailOptions, (err, res) => {
-    if(err){
-        res.send('error')
+
+transporter.sendMail(mailOptions, (error, res)=>{
+
+    if(error){
+        res.send(error)
     }
     else{
         res.send('Success')
     }
-})  
 
-smtpTransport.close();
+    transporter.close();
+})
+
+
+
+
+
 
 })
 
-// Tell the app to listen on port 3001
-const port = process.env.PORT || 3001;
-app.listen(port, function () {
-    console.log(`Express is running on port: ${port}`);
-});
 
 
+const PORT = process.env.PORT||3001;
 
-
-
-
-
-
-
-
-
-// const transporter = nodemailer.createTransport({
-//     host: 'smtp.gmail.com', //replace with your email provider
-//     port: 465,
-//     auth: {
-//         user: process.env.EMAIL,
-//         pass: process.env.PASSWORD
-//     }
-// });
-
-// verify connection configuration
-// transporter.verify(function(error, success) {
-//     if (error) {
-//         console.log(error);
-//     } else {
-//         console.log("Server is ready to take our messages");
-//     }
-// });
-
-// app.post('/api/forma', (req, res, next) => {
-// let name = req.body.name
-// let lastname = req.body.lastname
-// let email = req.body.email
-// let message = req.body.message
-
-// let mail = {
-//     from: name,
-//     to: process.env.EMAIL,
-//     // subject: subject,
-//     text: message,
-// }
-
-// transporter.sendMail(mail, (err, data) => {
-//     if (err) {
-//         res.json({
-//             status: 'fail'
-//     })
-// } else {
-//     res.json({
-//         status: 'success'
-//     })
-// }
-// })
-// })
+app.listen(PORT,()=>{
+    console.log(`server listening at port ${PORT}`);
+    
+})
